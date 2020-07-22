d2a4cccdf0e7894d058635c59cd2266d9b52d9e2
wisnupramoedya <wisnupramoedya@gmail.com>
2020-07-22 14:15:11 +0700

    Home and Controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="padding-top: 2rem">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-header">
+                    <span style="font-size: 1.5rem">{{ __('QUESTIONS') }}</span>
+                    <button class="float-right btn btn-success text-white rounded">Ask Question +</button>
                 </div>
+
+                <!-- List of question -->
+                @for ($i = 0; $i < 5; $i++)
+                <div class="card m-1">
+                    <div class="card-header">
+                        Wisnu
+                        <span class="float-right">
+                            <span class="fa fa-clock-o ml-1"></span>
+                            02:02:10
+                        </span>
+                    </div>
+                    <div class="card-body">
+                        <div>
+                            Siapa nama komting kita?
+                        </div>
+                        <button class="float-right btn btn-primary text-white">Find More ></button>
+                    </div>
+                </div>
+                @endfor
             </div>
         </div>
     </div>
